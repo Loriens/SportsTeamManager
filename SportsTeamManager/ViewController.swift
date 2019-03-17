4cb7b5ddc181902cb99ff304b1c4b8b46cf93bcc
Vladislav <lerd.95@mail.ru>
2019-03-17 17:58:29 +0300

    Initial Commit

---
+++ SportsTeamManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SportsTeamManager
+//
+//  Created by Vladislav on 17/03/2019.
+//  Copyright © 2019 Vladislav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
