c3406e6fff7601ac6541ce15b329beac25893739
Vladislav <lerd.95@mail.ru>
2019-03-18 17:43:10 +0300

    Add data to pickerView in PlayerViewController

--- SportsTeamManager/PlayerViewController/PlayerViewController.swift
+++ SportsTeamManager/PlayerViewController/PlayerViewController.swift
@@ -17,13 +17,56 @@ class PlayerViewController: UIViewController {
     @IBOutlet weak var pickerView: UIPickerView!
     
     var teamManager: TeamManager!
+    private var teams = [Team]()
+    private var positions = ["Goalkeeper", "Defender", "Midfielder", "Forward"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.pickerView.dataSource = self
+        self.pickerView.delegate = self
+        
+        teams = teamManager.fetchData(from: Team.self)
+    }
+    
+    @IBAction func teamButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func positionButtonPressed(_ sender: Any) {
     }
     
     @IBAction func saveButtonPressed(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
+    }
+    
+}
+
+extension PlayerViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return teams.count
+        case 1:
+            return positions.count
+        default:
+            return 0
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return teams[row].name
+        case 1:
+            return positions[row]
+        default:
+            return "empty"
+        }
     }
     
 }
