be9b682192c80a0c29f56d3289b5ef0aff24f951
Vladislav <lerd.95@mail.ru>
2019-03-19 23:44:21 +0300

    Add default players and teams

--- SportsTeamManager/MainViewController/ViewController.swift
+++ SportsTeamManager/MainViewController/ViewController.swift
@@ -23,16 +23,7 @@ class MainViewController: UITableViewController {
         let addButton = UIBarButtonItem(title: "Add (+)", style: .done, target: self, action: #selector(addButtonPressed(_:)))
         self.navigationItem.rightBarButtonItem = addButton
         
-        // Create teams, if there is no one team
-        let context = teamManager.getContext()
-        let teams = teamManager.fetchData(from: Team.self)
-        if teams.count == 0 {
-            teamManager.createObject(from: Team.self).name = "Barcelona"
-            teamManager.createObject(from: Team.self).name = "Real Madrid"
-            teamManager.createObject(from: Team.self).name = "Chelsea"
-        }
-        teamManager.save(context: context)
-        
+        fillPlayersAndTeams()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -85,6 +76,85 @@ class MainViewController: UITableViewController {
     
     private func fetchData() {
         players = teamManager.fetchData(from: Player.self)
+    }
+    
+    /// Create players, if there is no one
+    private func fillPlayersAndTeams() {
+        let context = teamManager.getContext()
+        
+        let players = teamManager.fetchData(from: Player.self)
+        if players.count == 0 {
+            let context = teamManager.getContext()
+            
+            let teams = teamManager.fetchData(from: Team.self)
+            print(teams)
+            
+            let player1 = teamManager.createObject(from: Player.self)
+            player1.age = 23
+            player1.fullName = "Zigam Alexeev"
+            player1.image = UIImage(named: "player1")
+            player1.nationality = "Slavyanin"
+            player1.number = "10"
+            player1.position = "Forward"
+            player1.team = teams[0]
+            
+            let player2 = teamManager.createObject(from: Player.self)
+            player2.age = 10
+            player2.fullName = "Ronaldo Junior"
+            player2.image = UIImage(named: "player2")
+            player2.nationality = "Portuguese"
+            player2.number = "43"
+            player2.position = "Goalkeeper"
+            player2.team = teams[1]
+            
+            let player3 = teamManager.createObject(from: Player.self)
+            player3.age = 40
+            player3.fullName = "Lebron James"
+            player3.image = UIImage(named: "player3")
+            player3.nationality = "Russian"
+            player3.number = "9"
+            player3.position = "Midfielder"
+            player3.team = teams[2]
+            
+            let player4 = teamManager.createObject(from: Player.self)
+            player4.age = 30
+            player4.fullName = "Kevin Durant"
+            player4.image = UIImage(named: "player4")
+            player4.nationality = "Englishman"
+            player4.number = "9"
+            player4.position = "Midfielder"
+            player4.team = teams[0]
+            
+            let player5 = teamManager.createObject(from: Player.self)
+            player5.age = 21
+            player5.fullName = "Valentin Guryev"
+            player5.image = UIImage(named: "player5")
+            player5.nationality = "Russian"
+            player5.number = "6"
+            player5.position = "Defender"
+            player5.team = teams[3]
+            
+            let player6 = teamManager.createObject(from: Player.self)
+            player6.age = 21
+            player6.fullName = "Ronaldo Sixers"
+            player6.image = UIImage(named: "player6")
+            player6.nationality = "Americanec"
+            player6.number = "6"
+            player6.position = "Defender"
+            player6.team = teams[2]
+            
+            teamManager.save(context: context)
+        }
+        
+        let teams = teamManager.fetchData(from: Team.self)
+        if teams.count == 0 {
+            teamManager.createObject(from: Team.self).name = "Barcelona"
+            teamManager.createObject(from: Team.self).name = "Real Madrid"
+            teamManager.createObject(from: Team.self).name = "Chelsea"
+            teamManager.createObject(from: Team.self).name = "Spartak"
+        }
+        
+        teamManager.save(context: context)
     }
 
 }
