bea84705168195c67a4b1ec9359043269bae7926
Vladislav <lerd.95@mail.ru>
2019-03-24 15:35:26 +0300

    Add func fetchDataWithController

--- SportsTeamManager/TeamManager.swift
+++ SportsTeamManager/TeamManager.swift
@@ -78,4 +78,22 @@ final class TeamManager {
         return fetchResult
     }
     
+    func fetchDataWithController<T: NSManagedObject>(for entity: T.Type, sectionNameKeyPath: String? = nil, predicate: NSCompoundPredicate? = nil) -> NSFetchedResultsController<T> {
+        let context = getContext()
+        let request = entity.fetchRequest() as! NSFetchRequest<T>
+        
+        let sortDescriptor = NSSortDescriptor(key: "category.name", ascending: true)
+        //
+        request.predicate = predicate
+        request.sortDescriptors = [sortDescriptor]
+        let controller = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: sectionNameKeyPath, cacheName: nil)
+        do {
+            try controller.performFetch()
+        } catch {
+            debugPrint("Could not fetch: \(error.localizedDescription)")
+        }
+        
+        return controller
+    }
+    
 }
